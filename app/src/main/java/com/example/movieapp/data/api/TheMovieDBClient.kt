34d71f55a4fa9813d244a88a34ce712f99bb4728
Elijah <elijate@gmail.com>
2020-08-11 12:51:47 +0300

    Add Movie Data Source

--- app/src/main/java/com/example/movieapp/data/api/TheMovieDBClient.kt
+++ app/src/main/java/com/example/movieapp/data/api/TheMovieDBClient.kt
@@ -14,6 +14,7 @@ const val POSTER_BASE_URL = "https://image.tmdb.org/t/p/w342"
 
 const val FIRST_PAGE = 1
 const val POST_PER_PAGE = 20
+
 object TheMovieDBClient {
     fun getClient(): TheMovieDBInterface {
 
