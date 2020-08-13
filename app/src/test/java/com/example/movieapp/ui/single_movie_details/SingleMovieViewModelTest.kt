c502f12ac9c94d6683798a54dd4eed71c0ed4834
Elijah <elijate@gmail.com>
2020-08-13 21:31:50 +0300

    Add testing libraries

---
+++ app/src/test/java/com/example/movieapp/ui/single_movie_details/SingleMovieViewModelTest.kt
@@ -0,0 +1,21 @@
+package com.example.movieapp.ui.single_movie_details
+
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class SingleMovieViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getMovieDetails() {
+    }
+
+    @Test
+    fun getNetworkState() {
+    }
+}
